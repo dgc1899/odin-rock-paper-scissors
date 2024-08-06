e1840bbe6dc4d5817e0017a1c281990ae76529c9
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2024-08-06 00:42:07 -0700

    Add choice logic for computer and human

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello, world!");
+function getComputerChoice() {
+    let computerChoice;
+    let random = Math.floor(Math.random() * 3);
+    if (random === 1) {
+        computerChoice = "Rock";
+    }
+    else if (random === 2) {
+        computerChoice = "Paper";
+    }
+    else {
+        computerChoice = "Scissors";
+    }
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("What is your choice?");
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        return "Invalid choice!";
+    }
+    return humanChoice;
+}
